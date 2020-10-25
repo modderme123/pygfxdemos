c8abe8a43633e359e9fe20822ae0ba133242c9f6
modderme123 <modderme123@gmail.com>
2020-10-25 15:41:45 -0700

    start using pycharm

--- v1.py
+++ v1.py
@@ -1,15 +1,119 @@
-from pyshader import python2shader, vec3, vec4, mat4
+import wgpu
+from pyshader import python2shader
+from pyshader import RES_INPUT, RES_OUTPUT
+from pyshader import vec2, vec3, vec4, i32
+import glfw
+from wgpu.gui.glfw import update_glfw_canvasses, WgpuCanvas
+import wgpu.backends.rs
 
+
+# noinspection PyUnusedLocal
 @python2shader
 def vertex_shader(
-    vertex_pos=("input", 0, vec3),
-    transform=("uniform", (0, 0), mat4),
-    out_pos=("output", "Position", vec4),
+        index: (RES_INPUT, "VertexId", i32),
+        pos: (RES_OUTPUT, "Position", vec4),
+        color: (RES_OUTPUT, 0, vec3),
 ):
-    out_pos = transform * vec4(vertex_pos, 1.0)
+    positions = [vec2(+0.0, -0.5), vec2(+0.5, +0.5), vec2(-0.5, +0.7)]
+    p = positions[index]
+    pos = vec4(p, 0.0, 1.0)
+    color = vec3(p, 0.5)
 
+
+# noinspection PyUnusedLocal
 @python2shader
-def fragment_shader_flat(
-    color=("uniform", (0, 1), vec3), out_color=("output", 0, vec4),
+def fragment_shader(
+        in_color: (RES_INPUT, 0, vec3),
+        out_color: (RES_OUTPUT, 0, vec4),
 ):
-    out_color = vec4(color, 1.0)  # noqa
+    out_color = vec4(in_color, 1.0)
+
+
+glfw.init()
+
+canvas = WgpuCanvas(title="wgpu triangle with GLFW")
+
+"""Regular function to setup a viz on the given canvas."""
+adapter = wgpu.request_adapter(canvas=canvas, power_preference="high-performance")
+device = adapter.request_device(extensions=[], limits={})
+
+vshader = device.create_shader_module(code=vertex_shader)
+fshader = device.create_shader_module(code=fragment_shader)
+
+bind_group_layout = device.create_bind_group_layout(entries=[])  # zero bindings
+bind_group = device.create_bind_group(layout=bind_group_layout, entries=[])
+
+pipeline_layout = device.create_pipeline_layout(
+    bind_group_layouts=[bind_group_layout]
+)
+
+render_pipeline = device.create_render_pipeline(
+    layout=pipeline_layout,
+    vertex_stage={"module": vshader, "entry_point": "main"},
+    fragment_stage={"module": fshader, "entry_point": "main"},
+    primitive_topology=wgpu.PrimitiveTopology.triangle_list,
+    rasterization_state={
+        "front_face": wgpu.FrontFace.ccw,
+        "cull_mode": wgpu.CullMode.none,
+        "depth_bias": 0,
+        "depth_bias_slope_scale": 0.0,
+        "depth_bias_clamp": 0.0,
+    },
+    color_states=[
+        {
+            "format": wgpu.TextureFormat.bgra8unorm_srgb,
+            "alpha_blend": (
+                wgpu.BlendFactor.one,
+                wgpu.BlendFactor.zero,
+                wgpu.BlendOperation.add,
+            ),
+            "color_blend": (
+                wgpu.BlendFactor.one,
+                wgpu.BlendFactor.zero,
+                wgpu.BlendOperation.add,
+            ),
+        }
+    ],
+    vertex_state={"index_format": wgpu.IndexFormat.uint32, "vertex_buffers": []},
+    sample_count=1,
+    sample_mask=0xFFFFFFFF,
+    alpha_to_coverage_enabled=False,
+)
+
+swap_chain = device.configure_swap_chain(
+    canvas,
+    device.get_swap_chain_preferred_format(canvas),
+    wgpu.TextureUsage.OUTPUT_ATTACHMENT,
+)
+
+
+def draw_frame():
+    with swap_chain as current_texture_view:
+        command_encoder = device.create_command_encoder()
+
+        render_pass = command_encoder.begin_render_pass(
+            color_attachments=[
+                {
+                    "attachment": current_texture_view,
+                    "resolve_target": None,
+                    "load_value": (0, 0, 0, 1),  # LoadOp.load or color
+                    "store_op": wgpu.StoreOp.store,
+                }
+            ],
+        )
+
+        render_pass.set_pipeline(render_pipeline)
+        render_pass.set_bind_group(
+            0, bind_group, [], 0, 999999
+        )  # last 2 elements not used
+        render_pass.draw(3, 1, 0, 0)
+        render_pass.end_pass()
+        device.default_queue.submit([command_encoder.finish()])
+
+
+canvas.request_draw(draw_frame)
+
+while update_glfw_canvasses():
+    glfw.poll_events()
+
+glfw.terminate()
